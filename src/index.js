2f3e68255b9a97dd39ba13b69b6d7ab84d4aff40
papiroca-tm <danshin.workspace@gmail.com>
2016-11-08 02:13:48 +0300

    форматирование

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+
 const app = express();
 
 app.use(cors());
-app.get('/', (req, res) => {
+
+app.listen(3000, () => {
+  console.log('app listening on port 3000');
+});
+
+/*
+* task2A
+* */
+app.get('/task2A', (req, res) => {
   const a = req.query.a || 0;
   const b = req.query.b || 0;
   const result = parseInt(a) + parseInt(b);
   res.send(result.toString());
 });
 
-app.listen(3000, () => {
-  console.log('app listening on port 3000');
-});
+
