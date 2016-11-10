4b974d215597ef47d41009fc7d08b5164b2a564a
papiroca-tm <danshin.workspace@gmail.com>
2016-11-10 20:48:33 +0300

    task2b not full

--- src/index.js
+++ src/index.js
@@ -13,10 +13,54 @@ app.listen(3000, () => {
 * task2A
 * */
 app.get('/task2A', (req, res) => {
+
   const a = req.query.a || 0;
   const b = req.query.b || 0;
   const result = parseInt(a) + parseInt(b);
   res.send(result.toString());
+
 });
+
+/*
+ * task2B
+ * */
+app.get('/task2B', (req, res) => {
+
+  const fullname = req.query.fullname;
+  const arr = fullname.split(' ');
+
+  let result = 'Invalid fullname';
+  const len = arr.length;
+
+
+
+  if (len == 1 && arr[0] !== '') {
+    const sName = arr[0];
+    result = `${sName}`;
+  } else if  (len == 2) {
+    const sName = arr[1];
+    const fName = char2UCase(arr[0]);
+    result = `${sName} ${fName}`;
+  } else if (len == 3) {
+    const sName = arr[2];
+    const fName = char2UCase(arr[0]);
+    const tName = char2UCase(arr[1]);
+    result = `${sName} ${fName} ${tName}`;
+  }
+
+  // arr.forEach(word => {
+  //   console.log(word.match( /^[a-zа-яё]+$/ig ));
+  //   if (word.match( /^[a-zа-яё]+$/ig ) == null) {
+  //     result = 'Invalid fullname';
+  //   }
+  // });
+
+  res.send(result.toString());
+
+});
+
+function char2UCase(str) {
+  return (str) ? str.charAt(0).toUpperCase() + '.': '';
+}
 
 
